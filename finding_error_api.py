714494e68d29e624fc9f2066c0e82afa269a2423
raagavendran <raagavendran@gmail.com>
2017-05-16 17:57:24 +0530

    Dashboard Test Completed

--- finding_error_api.py
+++ finding_error_api.py
@@ -1,40 +1,42 @@
 import requests
+import getpass
 
-
-url = "url/api/dashboard/"
-
-t=requests.get(url,auth=('uname','pwd'))
-data = t.json()
-
+host = raw_input('Enter host address: ')
+url = host + "/api/dashboard/"
 list_of_id = list()
 list_of_users = list()
 
-for item in data['result']:
-    list_of_id.append(item['id'])
+cokkie_session = requests.Session()
+uname = raw_input('Enter username: ')
+pword =getpass.getpass()
+cokkie_session.auth = (uname,pword)
+t = cokkie_session.get(url)
+print cokkie_session.cookies
+dashboard_data = t.json()
 
-print list_of_id
-
+for passing_id in dashboard_data['result']:
+    list_of_id.append(passing_id['id'])
+print "Total number of Custom Dashboard: "+ str(len(list_of_id))
+print "List of Custom Dashboard ID: "+str(list_of_id)
 
 for a in list_of_id:
-    open_id = "url/api/dashboard/" + str(a)
-    open_id_req = requests.get(open_id,auth=('uname','pwd'))
-    open_id_data = open_id_req.json()
-    # print open_id_req.json()
-    for b in open_id_data['objects']:
-        list_of_users.append(b['data'])
 
-print list_of_users
-
-
-for c in list_of_users:
-    open_final = "url" + str(c)
-    open_final_req = requests.get(open_final,auth=('uname','pwd'))
-    if (open_final_req.ok):
-        print "working" + c
-
-    else:
-        open_final_req.raise_for_status()
-
-
-# print data_list
-# myResponse.raise_for_status()
+        list_of_users = []
+        open_id = url + str(a)
+        open_id_req = cokkie_session.get(open_id)
+        open_id_data = open_id_req.json()
+        print '*'* 100
+        print "Checking dashboard: "+ str(open_id_data['name'])
+        print '_' * 100
+        for b in open_id_data['objects']:
+            list_of_users.append(b['data'])
+        for c in list_of_users:
+            print "Checking :" + c
+            try:
+                open_final = host + str(c)
+                open_final_req = cokkie_session.get(open_final)
+                open_final_req.raise_for_status()
+                if (open_final_req.ok):
+                    print "status OK"
+            except requests.HTTPError as err:
+                print "status ERROR"
